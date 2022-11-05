4f2e889464dc09fc885afe99fd7afc05d833fca1
Vipy18 <87119267+Vipy18@users.noreply.github.com>
2022-11-05 18:58:25 +0530

    Update cmd.py
    Achar Khare bro isme ab T_T

--- cmd.py
+++ cmd.py
@@ -4,27 +4,29 @@ import eel
 from cryptography.fernet import Fernet
 from datetime import datetime
 
-Database = pd.read_csv('pass.csv')
+Database = pd.read_pickle('pass')
+# The data in encrypted form has its own datatype and class, if we save it to CSV, it gets turned into string.
+# String cannot be decrypted by library as it is.
+# To prevent that, we're using to_pickle().
+
 
 # to add an id just put Add(user,pswd,site)
-def Add(user,pswd,site):
-    key = Fernet.generate_key()
-    fernet = Fernet(key)
+def Add(user, pswd, site) :
+    pkey = Fernet.generate_key()
+    fernet = Fernet(pkey)
     now = datetime.now()
-    Database.at[user,:] = [fernet.encrypt(pswd.encode()),key,site,now.strftime("%d/%m/%Y %H:%M:%S"),]
-    Database.to_csv('pass.csv')
+    pswdencd = fernet.encrypt(pswd.encode())
+    curtime = now.strftime("%d/%m/%Y %H:%M:%S")
+    Database.loc[user, :] = [pswdencd, fernet, site, curtime]
+    # We can't store Key as it in Database.
+    # The Fernet(pkey) value generated everytime, even for same key is different
+    # so we're storing Fernet(pkey) as it is
+    Database.to_pickle('pass')
+
 
 # to get data back use Collect(user)
-def Collect(user):
+def Collect(user) :
     pswd = Database.Pass[user]
-    key = Database.Key[user]
-    fernet = Fernet(key)
-    decoded_pass = fernet.decrypt(pswd).decode()
-    return [decoded_pass,Database.Site[user],Database.DnT[user]]
-
-
-
-
-
-    
-
+    pfer = Database.Key[user]
+    decoded_pass = pfer.decrypt(pswd).decode()
+    return [decoded_pass, Database.Site[user], Database.DnT[user]]
