85e6a384bb79159aabda46a061c3d6b31ff6635e
Vipul Deshmukh <vipulvdeshmukh@gmail.com>
2022-11-06 20:46:18 +0530

    Initializing 'pass' file
    Need a Sample 'pass' file for it. It is available in 'Clear Data' folder

---
+++ init.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from cryptography.fernet import Fernet
+
+# Use this to reset or initialize pass file
+# A sample or Original Pass file is needed for this
+print(r"You need to enter access key, it'll be used to extract you password. Be sure to remeber it")
+pswd = input('Access Key:\n')
+pkey = Fernet.generate_key()
+fernet = Fernet(pkey)
+pswdencd = fernet.encrypt(pswd.encode())
+B = pd.read_pickle('pass')
+for user in B.index:
+    B = B.drop(user, axis=0)
+
+B.at['MastPass', 'Pass'] = pswdencd
+B.at['MastPass', 'Site'] = 'root'
+B.at['MastPass', 'Key'] = fernet
+print(B)
+B.to_pickle('pass')
